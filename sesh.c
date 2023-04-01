815f4c35b562c535cf41fe3361ae18effd3ebbbe
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2023-04-01 15:09:52 -0400

    fix prompt str

--- sesh.c
+++ sesh.c
@@ -23,10 +23,10 @@ int main(int argc, char** argv){
   while(1){
     // terminal prompt
   loop:
-    memset(str, 0, sizeof(str)); // zero init cwd str
+    memset(str, 0, sizeof(str)); // zero init prompt str
     gethostname(str, 16);
     str[strlen(str)] = ':';
-    getcwd(&str[strlen(str)+1], MAX_STR);
+    getcwd(&str[strlen(str)], MAX_STR);
     write(1, str, sizeof(str));
     write(1, "$ ", 2);
 
